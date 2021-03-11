c84e3f7d44b7d649dc41adcc2c39ca84bce6faa6
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-11 19:24:04 -0300

    Configurando o djangorestframework (autenticação, permissão, paginação e requisição) no arquivo 'settings.py'

--- drf/settings.py
+++ drf/settings.py
@@ -135,5 +135,8 @@ STATIC_MEDIA = os.path.join(BASE_DIR, 'staticfiles')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
 }
