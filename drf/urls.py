583d596d36c21d1c5ae0ee3abad7351e8c4643a6
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-11 19:43:01 -0300

    Caminho pra API adicionado no 'urls.py'

--- drf/urls.py
+++ drf/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('auth/', include('rest_framework')),
+    path('api/v1/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
