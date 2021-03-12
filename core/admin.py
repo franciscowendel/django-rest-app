dbc6912d2a6a81ba729b927b2b322945cc33b3bd
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-12 02:08:01 -0300

    Models registrados no admin

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,16 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from .models import Curso, Avaliacao
+
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = (
+        'titulo', 'url', 'criado', 'modificado', 'ativo'
+    )
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = (
+        'curso', 'nome', 'email', 'comentario', 'avaliacao', 'criado', 'modificado', 'ativo'
+    )
