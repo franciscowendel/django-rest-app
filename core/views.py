82284667de38c0ebf52a1192b86d5d1296e5688b
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-11 22:01:52 -0300

    Criando a versão 1 das Views do projeto (usando generics)

--- core/views.py
+++ core/views.py
@@ -1,1 +1,8 @@
-from rest_framework import generics  # noqa
+from rest_framework import generics
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()  # noqa
+    serializer_class = CursoSerializer
