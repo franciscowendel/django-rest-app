bf2caf1e8e0d8770898ccc6052c4f9467cc4a22c
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-11 21:10:23 -0300

    Criando models no projeto

--- core/models.py
+++ core/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Base(models.Model):
     criado = models.DateField('Criado', auto_now_add=True)
-
+    modificado = models.DateField('Modificado', auto_now=True)
+    ativo = models.BooleanField()
