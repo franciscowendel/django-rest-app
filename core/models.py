21fcd3fa7f986d59a0545c24c4f90949365b855d
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-11 21:16:41 -0300

    Implementando models no projeto

--- core/models.py
+++ core/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Base(models.Model):
     criado = models.DateField('Criado', auto_now_add=True)
     modificado = models.DateField('Modificado', auto_now=True)
-    ativo = models.BooleanField()
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
