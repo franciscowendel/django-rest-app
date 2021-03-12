324e32462f6af491641f44fb697753dd28fdc9f6
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-11 22:18:41 -0300

    Esqueci de colocar 'urls' no caminho do rest_framework!

--- drf/urls.py
+++ drf/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('auth/', include('rest_framework')),
+    path('auth/', include('rest_framework.urls')),
     path('api/v1/', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
