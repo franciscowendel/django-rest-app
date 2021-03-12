b6a51ea02ea026827b9d890c1ec78074287303b5
franciscowendel <iamfranciscowendel@gmail.com>
2021-03-12 02:42:02 -0300

    media_avaliacoes adicionada no serializers de curso usando o SerializerMethodField

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
+from django.db.models import Avg
 
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
@@ -28,6 +29,8 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 class CursoSerializer(serializers.ModelSerializer):
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
 
     class Meta:
@@ -40,4 +43,12 @@ class CursoSerializer(serializers.ModelSerializer):
             'modificado',
             'ativo',
             'avaliacoes',
+            'media_avaliacoes',
         )
+
+    def get_media_avaliacoes(self, obj):  # noqa
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
